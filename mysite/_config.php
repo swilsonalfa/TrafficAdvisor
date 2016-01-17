0ecc0ca68b4a4f5416347451c19eeac5cc8c2bcb
Stewart Wilson <stewartwilson@outlook.com>
2016-01-17 15:57:48 +1100

    config

---
+++ mysite/_config.php
@@ -0,0 +1,8 @@
+<?php
+global $project;
+$project = 'mysite';
+global $database;
+$database = 'trafficadvisor';
+require_once('conf/ConfigureFromEnv.php');
+// Set the site locale
+i18n::set_locale('en_US');
