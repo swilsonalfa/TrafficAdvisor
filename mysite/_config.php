fd00f5b9ad6d5f792bad7f111138c48429616693
Stewart Wilson <swilsonalfa@users.noreply.github.com>
2016-07-31 20:17:31 +1000

    Update _config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -6,3 +6,5 @@ $database = 'trafficadvisor';
 require_once('conf/ConfigureFromEnv.php');
 // Set the site locale
 i18n::set_locale('en_US');
+
+$databaseSessionHandler = new SilverStripe\Predis\RedisSessionHandler();
